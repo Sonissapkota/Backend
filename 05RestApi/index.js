046d6430ebea9b00e1a7bc4d2ee3d5753447aaf1
Sonissapkota <sonishsapkota47@gmail.com>
2024-11-16 16:40:10 +0545

    Created some restful API

--- 05RestApi/index.js
+++ 05RestApi/index.js
@@ -1,6 +1,29 @@
-const express = require("express")
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+const PORT = 8000;
 
-const app = express()
-const PORT = 8000
+app.get('/api/users', (req,res)=>{
+    return res.json(users);
+})
 
-app.listen(PORT, ()=>console.log("Server Started"))
+app.get('/users', (req,res)=>{
+    const html = `
+    <ul>
+        ${users.map((user)=> `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+})
+
+app.get('/api/users/:id', (req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+
+app.get('/', (req,res)=>{
+    return res.end("This is home page");
+})
+
+app.listen(PORT, ()=>console.log("Server Started"));
